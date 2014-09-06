e6af92f7ec05ff1350a0660ad39c35f3111f8e90
Swami Atma <swami@ohlalaweb.com>
2014-09-06 12:35:40 +0200

    First implementation of the search feature.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,24 @@ class SearchController < ApplicationController
 
   def index
     @query = params[:query].downcase
+    @models_to_index = params[:models_to_index].split
+    search_terms = []
+    @query.split.each do |query_word|
+      search_terms << TkhSearchTerm.find_by( word: query_word )
+    end
+    instance_ids = []
+    if search_terms.any?
+      search_terms.each do |search_term|
+        search_term.tkh_search_instances.each do |search_instance|
+          instance_ids << search_instance.id if @models_to_index.include? search_instance.model_name
+        end
+      end
+    end
+    if instance_ids.any?
+      @results = TkhSearchInstance.where(id: instance_ids).by_top_rating.limit(25)
+    else
+      @results = []
+    end
   end
 
   def index_all_models
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    get 'search' => 'search#index'
+    post 'search' => 'search#index'
     get 'index_all_models' => 'search#index_all_models'
   end
 end
