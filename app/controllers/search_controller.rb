ea4cc06ec7192075c5277693faec5e067e222583
Swami Atma <swami@ohlalaweb.com>
2014-09-07 10:35:07 +0200

    Some refactoring.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
   def index
     @query = params[:query].downcase
-    @models_to_index = params[:models_to_index].split
+    @models_to_search = params[:models_to_search].split
     search_terms = []
     @query.split.each do |query_word|
       search_terms << TkhSearchTerm.find_by( word: query_word )
@@ -11,7 +11,7 @@ class SearchController < ApplicationController
     if search_terms.any?
       search_terms.each do |search_term|
         search_term.tkh_search_instances.each do |search_instance|
-          instance_ids << search_instance.id if @models_to_index.include? search_instance.model_name
+          instance_ids << search_instance.id if @models_to_search.include? search_instance.model_name
         end
       end
     end
@@ -25,7 +25,7 @@ class SearchController < ApplicationController
   def index_all_models
     # to disable this, set initializer as an empty array
     TkhSearch::TkhSearchable.indexable_models.each do |model|
-      (Kernel.const_get model).reverse_indexing
+      Kernel.const_get(model).reverse_indexing
     end
     redirect_to root_path, notice: "All searchable models have been indexed."
   end
--- app/models/tkh_search_instance.rb
+++ app/models/tkh_search_instance.rb
@@ -5,4 +5,8 @@ class TkhSearchInstance < ActiveRecord::Base
   scope :a_bit_old, -> { where('updated_at <= ?', ( Time.now - 2.minutes )) }
   scope :by_top_rating, -> { order('rating desc') }
 
+  def unique_record_name
+    "#{model_name}-#{model_record_id}"
+  end
+
 end
