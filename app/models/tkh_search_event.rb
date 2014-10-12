98db066d6614ce9ea80e9a3aaf6f825bb1690e9a
Swami Atma <swami@TenThousandHours.eu>
2014-10-12 15:18:39 +0200

    Search stats show last month's most popular queries in frequency order.

--- app/models/tkh_search_event.rb
+++ app/models/tkh_search_event.rb
@@ -2,6 +2,7 @@ class TkhSearchEvent < ActiveRecord::Base
 
   belongs_to :tkh_search_query
 
-
+  scope :last_24_hours, -> { where("created_at >= ?", Time.zone.now - 24.hours ) }
+  scope :last_month, -> { where("created_at >= ?", Time.zone.now - 1.month ) }
 
 end
