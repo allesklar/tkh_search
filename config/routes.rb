f0e91675155662547343654c917f8c9d28fd83fe
Swami Atma <swami@TenThousandHours.eu>
2015-03-24 18:18:49 +0100

    Fixed up routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    localized do
-      post 'search' => 'search#index'
-      get 'index_all_models' => 'search#index_all_models'
-      get 'search_stats' => 'search_stats#index'
-    end
+    post 'search' => 'search#index'
+    get 'index_all_models' => 'search#index_all_models'
+    get 'search_stats' => 'search_stats#index'
   end
 end
