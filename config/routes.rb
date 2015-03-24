674a51c04ac84c7fb4f21735c9c6db0e1cf550ba
Swami Atma <swami@TenThousandHours.eu>
2015-03-24 18:22:14 +0100

    Changed routes to be compatible with route_translator gem.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+  # These routes are compatible with the route_translator gem
+  # without the localized block, the controller would not return the proper current locale
+  localized do
     post 'search' => 'search#index'
     get 'index_all_models' => 'search#index_all_models'
     get 'search_stats' => 'search_stats#index'
