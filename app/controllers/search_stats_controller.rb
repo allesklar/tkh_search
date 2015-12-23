3976c98819631149e26c6c2a8806a3e37cf2c7e3
Swami Atma <swami@TenThousandHours.eu>
2015-12-23 21:12:19 +0100

    Renamed require_permission method to require_permission_to.

--- app/controllers/search_stats_controller.rb
+++ app/controllers/search_stats_controller.rb
@@ -1,7 +1,7 @@
 class SearchStatsController < ApplicationController
 
   before_action :authenticate
-  before_action -> { require_permission 'read_search_stats'}
+  before_action -> { require_permission_to 'read_search_stats'}
 
   def index
     render :layout => 'admin'
